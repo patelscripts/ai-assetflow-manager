f0e5e610a97fef7c66e369f53ae908dd867707fb
patelscripts <navneetpatel325@gmail.com>
2026-07-12 09:39:04 +0530

    folder structure and mongodb connection completed

---
+++ backend/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectDb = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("mongodb connected")
+    }catch(error){
+        console.error("mongodb connection failed", error.message);
+        process.exit(1);
+    }
+}
+export default connectDb;
---
+++ backend/server.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+import cors from 'cors';
+import connectDb from './config/db.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+connectDb();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
+    console.log(`server is running on the http://localhost:${PORT}`)
+})
